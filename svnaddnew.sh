f7070d4aa59ffdf6503d304eca003588fb809d59
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2010-09-20 14:09:09 -0400

    update svnaddnew script to properly handle whitespace

--- svnaddnew.sh
+++ svnaddnew.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-svn status | grep ^? | awk '{ $1=""; $0=$0; $1=$1; print;}' | xargs svn add
+old_ifs=${IFS}
+IFS=$'
+'
+svn status | grep ^? | awk '{ $1=""; $0=$0; $1=$1; print;}' | xargs -I{} svn add {}
+IFS=${old_ifs}
